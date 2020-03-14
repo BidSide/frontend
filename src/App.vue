1d74e7e5cecff59bf06643fa03ae21faaeaa14f9
richardszegh <richard.szegh@gmail.com>
2020-03-14 15:43:46 +0100

    declutter

--- src/App.vue
+++ src/App.vue
@@ -34,24 +34,15 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  name: 'App'
 });
 </script>
