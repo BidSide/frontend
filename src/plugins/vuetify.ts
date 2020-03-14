d8d8bbf9b164c7c02bd777f1899e4ba27c35b9ba
richardszegh <richard.szegh@gmail.com>
2020-03-14 17:43:46 +0100

    added new logos

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,16 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#700961',
+        secondary: '#b80d57',
+        error: '#e03e36',
+        warning: '#ff7c38',
+        info: '#1eb2a6'
+      }
+    }
+  }
+});
