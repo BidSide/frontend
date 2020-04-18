f3515f935363c51a830e7871d72f068a0fa12674
richardszegh <richard.szegh@gmail.com>
2020-04-18 16:40:22 +0200

    f(ProductList): added

---
+++ src/types/response/product.d.ts
@@ -0,0 +1,9 @@
+export default interface Product {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  starterPrice: number;
+  currentPrice: number;
+  buyoutPrice: number;
+}
