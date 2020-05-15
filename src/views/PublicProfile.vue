45ec0714435650fb30df97bf4cb20b3b565b5c3c
richardszegh <richard.szegh@gmail.com>
2020-05-15 19:11:06 +0200

    feat(public-profile): PublicProfile component & route

---
+++ src/views/PublicProfile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Public profile test</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  metaInfo: {
+    title: 'View profile'
+  }
+})
+export default class PublicProfile extends Vue {}
+</script>
