ee5380c02fcb82243b4d52f23fae0e5733b10364
richardszegh <richard.szegh@gmail.com>
2020-05-15 15:41:35 +0200

    enh(profile/my-transactions): prettifying

--- src/components/Profile/MyTransactions.vue
+++ src/components/Profile/MyTransactions.vue
@@ -1,10 +1,28 @@
 <template>
   <v-list :dense="$vuetify.breakpoint.xsOnly">
-    <v-list-item v-for="(transaction, index) in transactions" :key="index">
+    <v-list-item
+      v-for="(transaction, index) in sortedTransactions"
+      :key="index"
+      three-line
+    >
       <v-list-item-content>
-        <v-list-item-title>
-          {{ transaction.reason }}
+        <v-list-item-title class="font-weight-bold">
+          {{
+            isTransactionPositive(transaction.reason) === undefined
+              ? transaction.amount
+              : isTransactionPositive(transaction.reason)
+              ? `+${transaction.amount}`
+              : `-${transaction.amount}`
+          }}
         </v-list-item-title>
+
+        <v-list-item-subtitle>
+          {{ transaction.reason | formatReason }}
+        </v-list-item-subtitle>
+
+        <v-list-item-subtitle>
+          {{ transaction.createdAt | formatDate }}
+        </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </v-list>
@@ -14,15 +32,69 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+import { parseISO, format } from 'date-fns';
 
 import { TransactionLog } from '@/types';
 
-@Component
+@Component({
+  filters: {
+    formatReason: function(reason: string): string {
+      switch (reason) {
+        case 'WALLET_TOPUP':
+          return 'Top-up balance';
+        case 'BUYOUT':
+          return 'Buying a product';
+        case 'BID_LOCKDOWN':
+          return 'Making bid';
+        case 'OUTBID_CREDITING':
+          return 'Return locked credit (someone outbid you)';
+        default:
+          return 'Unknown reason';
+      }
+    },
+    formatDate(dateString: string): string {
+      const date = parseISO(dateString);
+
+      return format(date, 'dd/MM/yyyy HH:mm');
+    }
+  }
+})
 export default class MyTransactions extends Vue {
   @Prop({
     type: Array,
     required: true
   })
   readonly transactions!: TransactionLog[];
+
+  mounted() {
+    console.log(this.transactions);
+  }
+
+  get sortedTransactions(): TransactionLog[] {
+    return this.transactions.sort((a, b) => {
+      if (a.createdAt < b.createdAt) {
+        return 1;
+      } else if (b.createdAt < a.createdAt) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  isTransactionPositive(reason: string): boolean | undefined {
+    switch (reason) {
+      case 'WALLET_TOPUP':
+        return true;
+      case 'BUYOUT':
+        return false;
+      case 'BID_LOCKDOWN':
+        return false;
+      case 'OUTBID_CREDITING':
+        return true;
+      default:
+        return undefined;
+    }
+  }
 }
 </script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -165,7 +165,6 @@ export default class Profile extends Vue {
 
   mounted() {
     this.fetchProfile().then(() => {
-      console.log(this.profile);
       this.fetchMyProducts();
     });
   }
